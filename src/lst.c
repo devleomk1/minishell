e845020208861804f03294f00684434bdb2a3ab9
devleomk1 <wltnskjs@naver.com>
2021-10-25 13:43:19 +0900

    🚧 Work in progress
    for RESET cluster iMac

--- src/lst.c
+++ src/lst.c
@@ -6,9 +6,15 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 20:12:15 by jisokang          #+#    #+#             */
-/*   Updated: 2021/10/24 20:33:14 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/10/25 13:43:14 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+typedef struct s_lst
+{
+	char			*value;
+	struct s_lst	*next;
+}					t_lst;
 
 /* linked list
  * ----------------
@@ -19,6 +25,47 @@
  * - lst_del
  * ... 등등드읃응
  * */
+
+t_lst	*lst_new(int value)
+{
+	t_lst	*new;
+
+	new = (t_lst *)malloc(sizeof(t_lst));
+	if (new == NULL)
+	{
+		ft_putstr_fd("Malloc error\n", 2);
+		return (NULL);
+	}
+	new->value = value;
+	new->next = new;
+	return (new);
+}
+
+void	lst_insert(t_lst *before, t_lst *new)
+{
+	new->prev = before;
+	new->next = before->next;
+	before->next->prev = new;
+	before->next = new;
+}
+
+void	dlst_clear(t_lst *head)
+{
+	t_lst	*curr;
+	t_lst	*next;
+
+	curr = head;
+	if (!curr)
+		return ;
+	while (1)
+	{
+		next = curr->next;
+		free(curr);
+		curr = next;
+		if (curr == head)
+			return ;
+	}
+}
 
 /* list -> **str function */
 /* list <- **str function */
